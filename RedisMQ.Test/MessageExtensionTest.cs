d42ec22fa556eb408b02fc94e49d5a0e612ab2ab
LiZhengHao <1263212577@qq.com>
2023-01-20 15:15:02 +0800

    初始化测试项目

---
+++ RedisMQ.Test/MessageExtensionTest.cs
@@ -0,0 +1,76 @@
+﻿using LZH.RedisMQ.Messages;
+
+namespace RedisMQ.Test
+{
+    public class MessageExtensionTest
+    {
+        [Fact]
+        public void GetIdTest()
+        {
+            var msgId = Guid.NewGuid().ToString();
+            var header = new Dictionary<string, string>()
+            {
+                [Headers.MessageId] = msgId
+            };
+            var message = new Message(header, null);
+            
+            Assert.NotNull(message.GetId());
+            Assert.Equal(msgId,message.GetId());
+        }
+
+        [Fact]
+        public void GetNameTest()
+        {
+            var msgName = Guid.NewGuid().ToString();
+            var header = new Dictionary<string, string>()
+            {
+                [Headers.MessageName] = msgName
+            };
+            var message = new Message(header, null);
+
+            Assert.NotNull(message.GetName());
+            Assert.Equal(msgName, message.GetName());
+        }
+
+        [Fact]
+        public void GetCallbackNameTest()
+        {
+            var callbackName = Guid.NewGuid().ToString();
+            var header = new Dictionary<string, string>()
+            {
+                [Headers.CallbackName] = callbackName
+            };
+            var message = new Message(header, null);
+
+            Assert.NotNull(message.GetCallbackName());
+            Assert.Equal(callbackName, message.GetCallbackName());
+        }
+
+        [Fact]
+        public void GetGroupTest()
+        {
+            var group = Guid.NewGuid().ToString();
+            var header = new Dictionary<string, string>()
+            {
+                [Headers.Group] = group
+            };
+            var message = new Message(header, null);
+
+            Assert.NotNull(message.GetGroup());
+            Assert.Equal(group, message.GetGroup());
+        }
+
+        [Fact]
+        public void HasExceptionTest()
+        {
+            var exception = "exception message";
+            var header = new Dictionary<string, string>()
+            {
+                [Headers.Exception] = exception
+            };
+            var message = new Message(header, null);
+
+            Assert.True(message.HasException());
+        }
+    }
+}
---
+++ RedisMQ.Test/SubscribeInvokerTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using LZH.RedisMQ;
+using LZH.RedisMQ.Internal;
+using LZH.RedisMQ.Messages;
+using LZH.RedisMQ.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RedisMQ.Test
+{
+    public class SubscribeInvokerTest
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public SubscribeInvokerTest()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging();
+            serviceCollection.AddSingleton<ISerializer, JsonUtf8Serializer>();
+            serviceCollection.AddSingleton<ISubscribeInvoker, SubscribeInvoker>();
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        private ISubscribeInvoker SubscribeInvoker => _serviceProvider.GetService<ISubscribeInvoker>();
+
+        [Fact]
+        public async Task InvokeTest()
+        {
+            var descriptor = new ConsumerExecutorDescriptor()
+            {
+                Attribute = new RedisSubscribeAttribute("fake.output.integer"),
+                ServiceTypeInfo = typeof(FakeSubscriber).GetTypeInfo(),
+                ImplTypeInfo = typeof(FakeSubscriber).GetTypeInfo(),
+                MethodInfo = typeof(FakeSubscriber).GetMethod(nameof(FakeSubscriber.OutputIntegerSub)),
+                Parameters = new List<ParameterDescriptor>()
+            };
+
+            var header = new Dictionary<string, string>()
+            {
+                [Headers.MessageId] = SnowflakeId.Default().NextId().ToString(),
+                [Headers.MessageName] = "fake.output.integer"
+            };
+            var message = new Message(header, null);
+            var context = new ConsumerContext(descriptor, message);
+
+            var ret = await SubscribeInvoker.InvokeAsync(context);
+            Assert.Equal(int.MaxValue, ret.Result);
+        }
+    }
+
+    public class FakeSubscriber : IRedisSubscribe
+    {
+        [RedisSubscribe("fake.output.integer")]
+        public int OutputIntegerSub()
+        {
+            return int.MaxValue;
+        }
+    }
+}
